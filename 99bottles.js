bcb4c77774cf58a30e794348855b0bae795cb936
Unknown <miaochutseng@gmail.com>
2019-05-20 20:03:15 +0200

    update 99bottles.js with another solution

--- 99bottles.js
+++ 99bottles.js
@@ -1,3 +1,4 @@
+// solution A
 var bottlesX = 99;
 while (bottlesX != 2){
     console.log(bottlesX.toString() + " bottles of beer on the wall, " + bottlesX.toString() + " bottles of beer.");
@@ -12,3 +13,30 @@ while (bottlesX != 2){
             console.log("Go to the store and buy some more, 99 bottles of beer on the wall.");
         }
 }
+
+// solution B
+
+var bottles = "bottles";
+var nums = 99;
+while (nums > 0) {
+    console.log(nums + " " + bottles + " of beer on the wall, " + nums + " " + bottles + " of beer.");
+    console.log("Take one down and pass it around, ");
+    nums = nums - 1;
+    if (nums > 0) {
+        if (nums === 1) {
+            bottles = "bottle";
+        }
+        console.log(nums + " " + bottles + " of beer on the wall.");
+        console.log("");
+    }
+    else {
+        if (nums < 1) {
+            bottles = "bottles";
+        }
+        console.log("no more " + bottles + " of beer on the wall.");
+        console.log("");
+        console.log("No more bottles of beer on the wall, no more bottles of beer.");
+        console.log("Go to the store and buy some more, 99 bottles of beer on the wall.");
+    }
+}
+

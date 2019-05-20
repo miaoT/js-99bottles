462b36f6a643ef3612ea41f6195ba2f6a6810297
Unknown <miaochutseng@gmail.com>
2019-05-20 19:50:42 +0200

    add 99bottles.js

---
+++ 99bottles.js
@@ -0,0 +1,14 @@
+var bottlesX = 99;
+while (bottlesX != 2){
+    console.log(bottlesX.toString() + " bottles of beer on the wall, " + bottlesX.toString() + " bottles of beer.");
+    bottlesX = bottlesX - 1;
+    console.log("Take one down and pass it around, " + bottlesX.toString() + " bottles of beer on the wall.");
+        if(bottlesX == 2){
+            console.log("2 bottles of beer on the wall, 2 bottles of beer.");
+            console.log("Take one down and pass it around, 1 bottle of beer on the wall.");
+            console.log("1 bottle of beer on the wall, 1 bottle of beer.");
+            console.log("Take one down and pass it around, no more bottles of beer on the wall.");
+            console.log("No more bottles of beer on the wall, no more bottles of beer.");
+            console.log("Go to the store and buy some more, 99 bottles of beer on the wall.");
+        }
+}
